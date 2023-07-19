ca26ddb2ee28a1b96371a36e49b9d0c9a9ce7c38
woo3145 <lcwoo3145@gmail.com>
2023-07-19 21:16:07 +0900

    Client/ 🐛 VideoBufferedBar 버그 수정
    1. width값이 buffered.end 값으로 되어있어서 잘못 표시된 거 수정

--- client/src/components/VideoController/VideoBufferedBar.tsx
+++ client/src/components/VideoController/VideoBufferedBar.tsx
@@ -6,16 +6,20 @@ const VideoBufferedBar = () => {
   );
 
   return (
-    <div>
+    <div className="relative w-full h-2">
       {bufferedRanges.map((range, idx) => {
         // 버퍼링바 조각의 첫 시작이 현재 영상시점 이후에 있으면 표시안함
         if (currentTime < range.start) return null;
+
+        const startPosition = (range.start / duration) * 100;
+        const endPosition = (range.end / duration) * 100;
+        const width = endPosition - startPosition;
         return (
           <div
             key={idx}
             style={{
-              left: `${(range.start / duration) * 100}%`,
-              width: `${(range.end / duration) * 100}%`,
+              left: `${startPosition}%`,
+              width: `${width}%`,
             }}
             className="absolute top-0 h-2 bg-gray-500 rounded-full"
           />
