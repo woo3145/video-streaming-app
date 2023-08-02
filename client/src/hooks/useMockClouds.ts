b4ffdeb703bd8cc02d4898f03ff19d6855c0fca2
woo3145 <lcwoo3145@gmail.com>
2023-08-02 10:14:06 +0900

    Client/ 🔥 Mock 댓글, 구름 삭제

--- client/src/hooks/useMockClouds.ts
+++
@@ -1,5 +0,0 @@
-import { mockClouds } from '../constants/mockClouds';
-
-export const useMockClouds = () => {
-  return { data: mockClouds };
-};
--- client/src/hooks/useMockComments.ts
+++
@@ -1,5 +0,0 @@
-import { mockComments } from '../constants/mockComments';
-
-export const useMockComments = () => {
-  return { data: mockComments };
-};
