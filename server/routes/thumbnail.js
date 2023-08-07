932e81c414ba3e287b7a7e7cae0a9f8e50cc121d
woo3145 <lcwoo3145@gmail.com>
2023-08-07 23:13:44 +0900

    Server/ 🐛 썸네일 파일 path 오류 해결

--- server/routes/thumbnail.js
+++ server/routes/thumbnail.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 // 썸네일 파일 전송
 router.get('/:filename', (req, res) => {
   const fileName = req.params.filename;
-  const imagePath = `${__dirname}/../thumbnail/${fileName}`;
+  const imagePath = path.join(__dirname, '../thumbnail', fileName);
   res.sendFile(imagePath);
 });
 
